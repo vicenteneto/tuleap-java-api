9bbc675556337589124f3a015c74cf701ea17e49
Vicente Neto <vicente.silva@embedded.ufcg.edu.br>
2016-02-28 22:50:21 -0300

    Implemented project listing

--- src/main/java/br/com/vicenteneto/api/tuleap/TuleapServer.java
+++ src/main/java/br/com/vicenteneto/api/tuleap/TuleapServer.java
@@ -1,14 +1,26 @@
 package br.com.vicenteneto.api.tuleap;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.google.gson.Gson;
+import com.mashape.unirest.http.HttpResponse;
 
 import br.com.vicenteneto.api.tuleap.client.TuleapClient;
+import br.com.vicenteneto.api.tuleap.domain.Project;
+import br.com.vicenteneto.api.tuleap.exceptions.TuleapClientException;
+import br.com.vicenteneto.api.tuleap.exceptions.TuleapServerException;
+import br.com.vicenteneto.api.tuleap.util.ConfigurationUtil;
 
 public class TuleapServer {
 
-	private TuleapClient TuleapClient;
+	private static final String LIMIT = ConfigurationUtil.getConfiguration("LIMIT");
+	private static final String OFFSET = ConfigurationUtil.getConfiguration("OFFSET");
+
+	private TuleapClient tuleapClient;
 	private Gson gson;
 
 	private TuleapServer() {
@@ -17,6 +29,39 @@ public class TuleapServer {
 
 	public TuleapServer(URI serverURI, String username, String password) {
 		this();
-		TuleapClient = new TuleapClient(serverURI, username, password);
+		tuleapClient = new TuleapClient(serverURI, username, password);
+	}
+
+	public List<Project> getProjects() throws TuleapServerException {
+		try {
+			String url = ConfigurationUtil.getConfiguration("URL_API_PROJECTS");
+			HttpResponse<String> response = tuleapClient.get(url);
+
+			Project[] projects = gson.fromJson(response.getBody(), Project[].class);
+			return Arrays.asList(projects);
+		} catch (TuleapClientException exception) {
+			throw new TuleapServerException(exception);
+		}
+	}
+
+	public List<Project> getProjects(int limit, int offset) throws TuleapServerException {
+		if (offset < 0) {
+			String message = ConfigurationUtil.getConfiguration("OFFSET_MUST_NOT_BE_NEGATIVE");
+			throw new TuleapServerException(message);
+		}
+
+		try {
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			parameters.put(LIMIT, limit);
+			parameters.put(OFFSET, offset);
+
+			String url = ConfigurationUtil.getConfiguration("URL_API_PROJECTS");
+			HttpResponse<String> response = tuleapClient.get(url, parameters);
+
+			Project[] projects = gson.fromJson(response.getBody(), Project[].class);
+			return Arrays.asList(projects);
+		} catch (TuleapClientException exception) {
+			throw new TuleapServerException(exception);
+		}
 	}
 }
